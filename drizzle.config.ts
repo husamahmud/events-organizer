221262894d825b7183f9ae2e50c5938620bf0888
husamahmud <husamahmud@gmail.com>
2024-10-26 17:30:33 +0300

    chore

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import type { Config } from 'drizzle-kit'
 
 export default {
-  schema: './src/db/schema.ts',
+  schema: './db/schema.ts',
   out: './migrations',
   driver: 'turso',
   dbCredentials: {
